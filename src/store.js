bdd011f7e741080b0805428e92d227117d1880e2
sergiobni <bni.sergio@gmail.com>
2024-03-06 12:43:57 +0100

    adding more state, combining reducers

--- src/store.js
+++ src/store.js
@@ -1,12 +1,18 @@
-import { createStore } from 'redux';
+import { combineReducers, createStore } from 'redux';
 
-const initialState = {
+const initialStateAccount = {
   balance: 0,
   loan: 0,
   loanPurpose: '',
 };
 
-function reducer(state = initialState, action) {
+const initialStateCustomer = {
+  fullName: '',
+  nationalID: '',
+  createdAt: '',
+};
+
+function accountReducer(state = initialStateAccount, action) {
   switch (action.type) {
     case 'account/deposit':
       return { ...state, balance: state.balance + action.payload };
@@ -32,8 +38,29 @@ function reducer(state = initialState, action) {
       return state;
   }
 }
+function customerReducer(state = initialStateCustomer, action) {
+  switch (action.type) {
+    case 'customer/createCustomer':
+      return {
+        ...state,
+        fullName: action.payload.fullName,
+        nationalID: action.payload.nationalID,
+        createdAt: action.payload.createdAt,
+      };
+    case 'customer/updateName':
+      return { ...state, fullName: action.payload };
+    default:
+      return state;
+  }
+}
 
-const store = createStore(reducer);
+//Combining reducers
+const rootReducer = combineReducers({
+  account: accountReducer,
+  customer: customerReducer,
+});
+
+const store = createStore(rootReducer);
 
 // store.dispatch({ type: 'account/deposit', payload: 500 });
 // console.log(store.getState());
@@ -70,4 +97,20 @@ store.dispatch(withdraw(200));
 console.log(store.getState());
 
 store.dispatch(requestLoan(1000, 'Buy a cheap car'));
+console.log(store.getState());
+
+function createCustomer(fullName, nationalID) {
+  return {
+    type: 'customer/createCustomer',
+    payload: { fullName, nationalID, createdAt: new Date().toISOString() },
+  };
+}
+
+function updateName(fullName) {
+  return { type: 'account/updateName', payload: fullName };
+}
+
+store.dispatch(createCustomer('Sergio', '2000P'));
+console.log(store.getState());
+store.dispatch(deposit(250));
 console.log(store.getState());
