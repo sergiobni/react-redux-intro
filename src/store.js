bc0a25a981af8edcadbb0a9a07d7100933a1acbb
sergiobni <bni.sergio@gmail.com>
2024-03-07 12:10:50 +0100

    API call with redux Thunks

--- src/store.js
+++ src/store.js
@@ -1,12 +1,18 @@
-import { combineReducers, createStore } from 'redux';
+import { applyMiddleware, combineReducers, compose, createStore } from 'redux';
 import accountReducer from './features/accounts/accountSlice';
 import customerReducer from './features/customers/customerSlice';
+import { thunk } from 'redux-thunk';
+import { composeWithDevTools } from '@redux-devtools/extension/';
 
 const rootReducer = combineReducers({
   account: accountReducer,
   customer: customerReducer,
 });
 
-const store = createStore(rootReducer);
+//Appling thunk
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
 
 export default store;
